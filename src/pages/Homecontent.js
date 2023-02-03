2c4f7bc4c57c6677705c56b4a1c1bf35aa487758
vignesh <vignesh296169@gmail.com>
2023-02-03 20:24:51 +0530

    text

--- src/pages/Homecontent.js
+++ src/pages/Homecontent.js
@@ -6,6 +6,24 @@ function Homecontent() {
   //     backgroundSize:"cover",
   //     backgroundposition: 'center'
   // }
+  const partone =[
+    {id:"01",title:'Minim nostrud '},
+    {id:"02",title:' do ex consequat'},
+    {id:"03",title:'Minim nostrud'},
+    {id:"04",title:' laboris id labore'},
+
+
+
+]
+const parttwo =[
+  {id:"05",title:'consectetur fugiat. '},
+  {id:"06",title:' ex consequat'},
+  {id:"07",title:'Minim nostrud'},
+  {id:"08",title:' laboris labore'},
+
+
+
+]
   return (
     <div>
       <div className="bg-pack-train bg-no-repeat bg-cover bg-blend-darken">
@@ -20,16 +38,17 @@ function Homecontent() {
                 velit id incididunt ipsum proident anim anim irure amet aliqua
                 incididunt. Pariatur in consequat adipisicing ad commodo.
               </p>
-              <button className="font-light font-landing text-center border px-3 py-1 hover:text-slate-500 active:skew-x-6 bg-gradient-to-tr rounded-lg from-cyan-500 to-indigo-700">
+              <button className="font-light font-landing text-center border px-3 py-1 active:text-slate-500 active:skew-x-6 bg-gradient-to-tr rounded-lg from-cyan-500 to-indigo-700">
                 LEARN MORE
               </button>
             </div>
           </div>
         </div>
       </div>
-      <div className="border-2 border-red-500">
-        <h3>ABOUT US</h3>
-        <p>
+      <div className=" px-2 py-4">
+        <div className="grid grid-col-1 gap-4">
+        <h3 className=" text-xl font-semibold font-landing border-b-4 w-1/12  border-solid border-blue-600" >ABOUT US</h3>
+        <p className="font-light font-landing ">
           In exercitation aliquip commodo nostrud consectetur nostrud deserunt
           non veniam qui irure pariatur sint. Ipsum dolore cupidatat velit
           voluptate occaecat sit fugiat ipsum. Sint ut anim mollit elit. Lorem
@@ -40,8 +59,18 @@ function Homecontent() {
           qui fugiat duis culpa labore minim eiusmod dolor.
         </p>
         <h4>Why Choose Us?</h4>
-        <div>
-
+        </div>
+        <div className=" p-1 mt-1 grid grid-cols-2 gap-2">
+         <div>
+          {partone.map((items)=>{
+           return <div key={items.id}><span className="rounded-md bg-blue-400 mr-2 text-sm text-blue-200">{items.id}</span>{items.title}</div>
+          })}
+         </div>
+         <div>
+           {parttwo.map((items)=>{
+            return <div key={items.id}> <span className="rounded-md bg-blue-400 mr-2 text-sm text-blue-200">{items.id}</span>{items.title}</div>
+           })}
+         </div>
         </div>
       </div>
     </div>
